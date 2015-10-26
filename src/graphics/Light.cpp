915519b2466d54e343b5f8b2ad6f9ba1c4ddf57f
LeJon McGowan <lejonmcgowan@gmail.com>
2015-10-25 22:20:37 -0700

    lighting attenuation

--- src/graphics/Light.cpp
+++ src/graphics/Light.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Light.h"
+const static float Quadratic_Attenuation = 75.0f;
+const static float Linear_Attenuation = 4.5;
+const static float Constant_Attenuation = 1.0f
+
+void Light::setLightRange(float range)
+{
+    this->range = range;
+    attenuation = glm::vec3(range * range / Quadratic_Attenuation,
+                            range / Linear_Attenuation,
+                            Constant_Attenuation);
+}
